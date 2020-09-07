7a5c0d8387a85b7d58e065afb14d88edbfdfcff6
Oswaldo Quiroz <oquiroz@gandalocal.com>
2020-09-07 16:18:17 -0430

    build done

---
+++ build/precache-manifest.43a7422875c1ca11161a8144cdf76f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43f6523d8071ba976b773a440fafaf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe077566d8e6bedffc6",
+    "url": "/static/css/main.b7e9e9f9.chunk.css"
+  },
+  {
+    "revision": "cb9cd0d71d8475f95b11",
+    "url": "/static/js/2.b3629a38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3629a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe077566d8e6bedffc6",
+    "url": "/static/js/main.dbfedc2a.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
