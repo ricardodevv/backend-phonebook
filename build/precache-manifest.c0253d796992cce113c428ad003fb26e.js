1574ab39ca0b2340c91be6982d911c0a06816c68
Oswaldo Quiroz <oquiroz@gandalocal.com>
2020-09-07 16:31:38 -0430

    implementing frontend to backend 1

---
+++ build/precache-manifest.c0253d796992cce113c428ad003fb26e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b99da3f95cf2d1656e6882fc273d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "205916603b05caaa495f",
+    "url": "/static/css/main.b7e9e9f9.chunk.css"
+  },
+  {
+    "revision": "cb9cd0d71d8475f95b11",
+    "url": "/static/js/2.b3629a38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3629a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205916603b05caaa495f",
+    "url": "/static/js/main.99a2fb37.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
