c9583ef3906db113ad1b3862ad343c2536fa123e
Oswaldo Quiroz <oquiroz@gandalocal.com>
2020-09-08 12:49:52 -0430

    3003

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/api/peps', (request, response) => {
   }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
