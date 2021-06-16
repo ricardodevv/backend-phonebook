6d2227dcb0b47096268ccb0287c97a0060a884ff
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-06-16 14:50:15 -0430

    before pushing

---
+++ build/precache-manifest.b402f2213facdcff544e0cd763860468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcc103f621b207c92c80f12286e6972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55e0d790724b809fe892",
+    "url": "/static/css/main.b7e9e9f9.chunk.css"
+  },
+  {
+    "revision": "cb9cd0d71d8475f95b11",
+    "url": "/static/js/2.b3629a38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3629a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e0d790724b809fe892",
+    "url": "/static/js/main.69227cb5.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
